234dcda7d3f69e47e9b073c73c5b9ce3e918e710
Russ Cox <rsc@golang.org>
2017-01-20 04:28:01 +0000

    addproject github.com/attic-labs/noms

---
+++ src/cloud.google.com/.corpus.rev.go
@@ -0,0 +1,9 @@
+commit 81b7822b1e798e8f17bf64b59512a5be4097e966
+Author: Gary Elliott <garyelliott@google.com>
+Date:   Wed Jan 18 11:13:56 2017 -0500
+
+    bigtable/bttest: Support ValueRangeFilter and ConditionFilter
+    
+    Change-Id: I1e85c425b770bd6e5393b381f3bb6e367061b34c
+    Reviewed-on: https://code-review.googlesource.com/10390
+    Reviewed-by: Jonathan Amsterdam <jba@google.com>
---
+++ src/github.com/attic-labs/.corpus.url.noms
@@ -0,0 +1,1 @@
+https://github.com/attic-labs/noms
