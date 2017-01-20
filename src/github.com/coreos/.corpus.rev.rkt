37e525f4caeb6248be241792c85c72582a101f67
Russ Cox <rsc@golang.org>
2017-01-20 04:26:20 +0000

    addproject github.com/coreos/rkt

---
+++ src/github.com/coreos/.corpus.rev.rkt
@@ -0,0 +1,8 @@
+commit 535596b77567093f8f031d28b82a6d205c29664c
+Merge: 8f2332f eef3d74
+Author: Sergiusz Urbaniak <sur@coreos.com>
+Date:   Thu Jan 19 17:25:57 2017 +0100
+
+    Merge pull request #3545 from s-urbaniak/release-1.23.0
+    
+    rkt release v1.23.0
