8c2c3bb2110e10592dd885438cfe60b4bf2f1993
Russ Cox <rsc@golang.org>
2017-01-20 04:30:37 +0000

    addproject github.com/go-kit/kit

---
+++ src/github.com/apache/.corpus.rev.thrift
@@ -0,0 +1,8 @@
+commit e0ccbd6e62e14f32d7c5fe0f9cec6eff3259b863
+Author: Roger Meier <roger@apache.org>
+Date:   Mon Jan 16 20:26:57 2017 +0100
+
+    chore: install ruby-bundler
+    Fixes recent build failures in the Travis CI environment.
+    
+    This closes #1158
